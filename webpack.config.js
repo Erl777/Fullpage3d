58785a8d1510771181a91abaa9e839ffa5a789f9
kirboka <kir_boyko@bk.ru>
2021-10-31 11:45:30 +0300

    add babel with polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: {
-    main: './index.js'
+    main: ['@babel/polyfill' ,'./index.js', ]
   },
   output: {
     filename: filename('js'),
@@ -99,6 +99,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
